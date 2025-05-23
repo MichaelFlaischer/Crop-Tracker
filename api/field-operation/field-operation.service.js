77b10eac2ca366ad82ae8c9a95eebebed9d227fd
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2025-05-23 15:30:00 +0300

    Completed full CRUD implementation for Field Operations management (frontend + backend).

--- api/field-operation/field-operation.service.js
+++ api/field-operation/field-operation.service.js
@@ -1,30 +1,32 @@
 import { dbService } from '../../services/db.service.js'
 import { ObjectId } from 'mongodb'
 
+const COLLECTION_NAME = 'FieldOperations'
+
 export async function query() {
-  const collection = await dbService.getCollection('FieldOperations')
+  const collection = await dbService.getCollection(COLLECTION_NAME)
   return await collection.find().toArray()
 }
 
 export async function getById(operationId) {
-  const collection = await dbService.getCollection('FieldOperations')
+  const collection = await dbService.getCollection(COLLECTION_NAME)
   return await collection.findOne({ _id: new ObjectId(operationId) })
 }
 
 export async function add(operation) {
-  const collection = await dbService.getCollection('FieldOperations')
+  const collection = await dbService.getCollection(COLLECTION_NAME)
   const res = await collection.insertOne(operation)
   return { ...operation, _id: res.insertedId }
 }
 
 export async function update(operationId, operation) {
-  const collection = await dbService.getCollection('FieldOperations')
+  const collection = await dbService.getCollection(COLLECTION_NAME)
   delete operation._id
   await collection.updateOne({ _id: new ObjectId(operationId) }, { $set: operation })
   return { ...operation, _id: new ObjectId(operationId) }
 }
 
 export async function remove(operationId) {
-  const collection = await dbService.getCollection('FieldOperations')
+  const collection = await dbService.getCollection(COLLECTION_NAME)
   await collection.deleteOne({ _id: new ObjectId(operationId) })
 }
